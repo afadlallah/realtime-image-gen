2cd7104855b2aaa204a9b3c698f5a1225b12e46f
afadlallah <afadlallah@gmail.com>
2025-05-06 00:06:48 -0400

    Upgraded to Tailwindcss v4.1.5

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,13 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className='grid min-h-screen grid-rows-[20px_1fr_20px] place-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20'>
+    <div className='grid min-h-screen grid-rows-[20px_1fr_20px] place-items-center gap-16 p-8 pb-20 font-(family-name:--font-geist-sans) sm:p-20'>
       <main className='row-start-2 flex flex-col items-center gap-8 sm:items-start'>
         <Image priority alt='Next.js logo' className='dark:invert' height={38} src='/next.svg' width={180} />
-        <ol className='list-inside list-decimal text-center font-[family-name:var(--font-geist-mono)] text-sm sm:text-left'>
+        <ol className='list-inside list-decimal text-center font-(family-name:--font-geist-mono) text-sm sm:text-left'>
           <li className='mb-2'>
             Get started by editing{' '}
-            <code className='rounded bg-black/[.05] px-1 py-0.5 font-semibold dark:bg-white/[.06]'>app/page.tsx</code>.
+            <code className='rounded-sm bg-black/[.05] px-1 py-0.5 font-semibold dark:bg-white/[.06]'>app/page.tsx</code>.
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
